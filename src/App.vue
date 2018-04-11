9d23d8b6c6e3905ba3bac76ff1a8767c29239e55
Curtis Miller <curtis.miller@cycosoft.com>
2018-04-10 20:44:53 -0700

    making use of components

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,8 @@
 <template>
   <div id="app">
+    <div class="panel-heading">
+      <h3 class="panel-title">Current Library</h3>
+    </div>
       <div class="panel-body">
       <table class="table table-striped">
         <thead>
@@ -29,15 +32,17 @@
             <label for="bookTitle">Title:</label>
             <input type="text" id="bookTitle" class="form-control" v-model="newBook.title">
           </md-input-container>
-          <div class="form-group">
+          <md-input-container class="form-group">
             <label for="bookAuthor">Author:</label>
             <input type="text" id="bookAuthor" class="form-control" v-model="newBook.author">
-          </div>
-          <div class="form-group">
+          </md-input-container>
+          <md-input-container class="form-group">
             <label for="bookUrl">Url:</label>
             <input type="text" id="bookUrl" class="form-control" v-model="newBook.url">
-          </div>
-          <input type="submit" class="btn btn-primary" value="Add Book">
+          </md-input-container>
+          <md-button>
+            <input type="submit" class="btn btn-primary" value="Add Book">
+          </md-button>
         </form>
       </div>
     </div>
